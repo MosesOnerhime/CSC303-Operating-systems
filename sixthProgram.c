d9815b6978a168f093961e43224ea5847efdf7c5
Moses <useradd@Moses-PC>
2024-11-22 17:21:35 +0100

    commit

---
+++ sixthProgram.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+    // int a;
+    // float b;
+    // double c;
+    // char d;
+    char name[] = "Moses";
+
+    // printf("%p\n", name);
+
+    // puts("Loop: ");
+
+    for (int i = 0; i < 5; i++) {
+        printf("%c\n", name[i]);
+    }
+    
+    
+    // scanf("%s", name);
+
+    // printf("%i\n", a);
+
+    // puts("Memory of variables declared");
+    // printf("Address of a: %p\n", &a);
+    // printf("Address of b: %p\n", &b);
+    // printf("Address of c: %p\n", &c);
+    // printf("Address of d: %p\n", &d);
+
+    return 0;
+}
